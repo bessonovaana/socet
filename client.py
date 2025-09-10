fdee0eec2b8f9555b2f0611dc714813dbe6f6f6a
bessonovaana <nasbesso@gmai.com>
2025-09-10 10:59:31 +0700

    change

--- client.py
+++
@@ -1,9 +0,0 @@
-import socet
-
-client_socet=socet.socet(socet.AF_INET, socet.SOCK_STREAM)
-
-client_socet.connect(('localhost',12345))
-
-client_socket.sendall(b'Hello, server!')
-
-client_socket.close()
